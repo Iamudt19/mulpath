d5564696517781f5b99a93217f12b1559c11a646
Udit <iamudt19@gmail.com>
2026-08-16 15:12:46 +0530

    feat: install and integrate @vercel/analytics

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Analytics } from '@vercel/analytics/react';
 import MainLayout from './layouts/MainLayout';
 import { Home, Collector, Aggregator, Lab, Manufacturer, Verify } from './pages';
 
@@ -16,8 +17,10 @@ function App() {
           <Route path="verify/:id" element={<Verify />} />
         </Route>
       </Routes>
+      <Analytics />
     </BrowserRouter>
   );
 }
 
 export default App;
+
